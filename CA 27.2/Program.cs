00b22097ce6da9d35de3ca1deb0ff34a4d321d88
Clawiatura <maksimov19992805@icloud.com>
2024-04-29 13:04:49 +0200

    27.2 DZ

---
+++ CA 27.2/Program.cs
@@ -0,0 +1,48 @@
+﻿class BankAccount
+{
+    private decimal balance;
+
+    public BankAccount(decimal initialBalance)
+    {
+        balance = initialBalance;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        balance += amount;
+        Console.WriteLine("Пополнение счета: " + amount);
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (amount > balance)
+        {
+            Console.WriteLine("Недостаточно средств");
+        }
+        else
+        {
+            balance -= amount;
+            Console.WriteLine("Снятие наличных: " + amount);
+        }
+    }
+
+    public void CheckBalance()
+    {
+        Console.WriteLine("Текущий баланс: " + balance);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        BankAccount account = new BankAccount(1000);
+
+        account.CheckBalance();
+        account.Deposit(500);
+        account.CheckBalance();
+        account.Withdraw(200);
+        account.CheckBalance();
+        account.Withdraw(1500);
+    }
+}
